a72e3d00cac9fb15dd400c0dc2247a51d0c00cd6
BitCanna <39583369+BitCannaGlobal@users.noreply.github.com>
2022-06-29 13:07:15 +0200

    Update statesync_client_linux_new.sh

--- statesync_client_linux_new.sh
+++ statesync_client_linux_new.sh
@@ -54,7 +54,7 @@ then
   curl -s $GENESIS > $HOME/.bcna/config/genesis.json
   
  
-  NODE1_IP="178.62.236.228"
+  NODE1_IP="176.9.139.74"
   RPC1="http://$NODE1_IP"
   P2P_PORT1=36656
   RPC_PORT1=36657
