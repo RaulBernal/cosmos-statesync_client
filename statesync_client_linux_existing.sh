f6b4980531abc53a9a56d33681ee511805d63a7a
BitCanna <39583369+BitCannaGlobal@users.noreply.github.com>
2022-06-29 13:10:36 +0200

    Update statesync_client_linux_existing.sh

--- statesync_client_linux_existing.sh
+++ statesync_client_linux_existing.sh
@@ -30,7 +30,7 @@ then
   echo "\nClearing the data folder & P2P Address Book"
   bcnad unsafe-reset-all || cosmovisor unsafe-reset-all
 
-  NODE1_IP="178.62.236.228"
+  NODE1_IP="176.9.139.74"
   RPC1="http://$NODE1_IP"
   P2P_PORT1=36656
   RPC_PORT1=36657
