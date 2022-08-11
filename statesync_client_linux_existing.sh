6133a8d27aa5a05bdc5c1312958af18b97cf5cc6
Raul Bernal <bernal.raul@gmail.com>
2022-08-11 11:12:38 +0200

    bypass bug with `tendermint unsafe-reset-all --home $HOME/.bcna`

--- statesync_client_linux_existing.sh
+++ statesync_client_linux_existing.sh
@@ -28,7 +28,7 @@ read -p "ATTENTION! This script will clear the data folder (unsafe-reset-all) & 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo "\nClearing the data folder & P2P Address Book"
-  bcnad unsafe-reset-all || cosmovisor unsafe-reset-all
+  bcnad tendermint unsafe-reset-all --home $HOME/.bcna || tendermint unsafe-reset-all --home $HOME/.bcna
 
   NODE1_IP="176.9.139.74"
   RPC1="http://$NODE1_IP"
